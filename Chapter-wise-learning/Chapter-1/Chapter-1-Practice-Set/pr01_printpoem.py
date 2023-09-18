3b140fedda3b67b0bd412bfbb8097122c14706c4
Alarixc <127346578+Alarixc@users.noreply.github.com>
2023-09-18 20:57:43 +0530

    Create pr01_printpoem.py

---
+++ Chapter-wise-learning/Chapter-1/Chapter-1-Practice-Set/pr01_printpoem.py
@@ -0,0 +1,9 @@
+'''
+This is a Multi line comment
+'''
+# This is also a comment like the above one
+
+print('''Danny G rant Danny G rant
+DannY G RANT DANNG FG ERANT GASFG RANT 
+ARMAR FANNTY G ANT TG RANT RANT RANT RANT
+RANT ARANT ARANT TAN RANT ERANT RANT RANT''')
