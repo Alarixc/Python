0c01f0927deeed545109c31944741175ea5b3b80
Alarixc <127346578+Alarixc@users.noreply.github.com>
2023-09-18 21:23:56 +0530

    Add files via upload

---
+++ Chapter-wise-learning/Chapter-7/quick_quiz_printlistelements.py
@@ -0,0 +1,9 @@
+fruits=['Banana','Oranges','Watermelons','Pear']
+i=0
+while i<len(fruits):
+    print(fruits[i])
+    i+=1
+    
+
+
+
---
+++ Chapter-wise-learning/Chapter-7/quick_quiz_printnos.py
@@ -0,0 +1,6 @@
+x=int(input("Enter where you want to start from:\n"))
+y=int(input("Enter the number where you would like the printing to stop:\n"))
+while (x<=y):
+    print(x)
+    x+=1
+
