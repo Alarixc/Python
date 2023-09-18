ec5c9177ed30a50c9bbd1fe075465a75e3eec4cd
Alarixc <127346578+Alarixc@users.noreply.github.com>
2023-09-18 21:22:03 +0530

    Add files via upload

---
+++ Chapter-wise-learning/Chapter-6/quick_quiz_age18.py
@@ -0,0 +1,14 @@
+age=int(input("Enter your age:\n"))
+# if(age>=18):
+#     print("You are eligible to drive.Yes!")
+# else:
+#     print("You are not eligible to drive! Feck off kid!")
+
+#OR,THIS CAN ALSO BE WRITTEN AS:
+
+if age>=18:
+    print("You are eligible to drive.Yes!")
+else:
+    print("You are not eligible to drive! Feck off kid!")
+
+
